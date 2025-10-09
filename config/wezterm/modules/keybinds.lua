5a98ff609de6605a9c2a4b10927a7272ebf3ca69
Niko <niko.u@hotmail.fi>
2025-10-09 13:34:47 +0300

    update

--- config/wezterm/modules/keybinds.lua
+++ config/wezterm/modules/keybinds.lua
@@ -1,5 +1,16 @@
 local wezterm = require("wezterm")
+local sessionizer = require("modules.sessionizer")
 
 local M = {}
+
+M.keys = {
+	{
+		key = "f",
+		mods = "LEADER",
+		action = wezterm.action_callback(function(window, pane)
+			sessionizer.toggle(window, pane)
+		end),
+	},
+}
 
 return M
--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,13 +1,14 @@
 local wez = require("wezterm")
 local config = wez.config_builder()
-config.enable_wayland = true
+
 config.enable_tab_bar = false
 config.color_scheme = "Brogrammer"
-config.colors = {
+config.colors = { background = "black" }
 
-	background = "black",
-}
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 
 local keybinds = require("modules.keybinds")
 config.keys = keybinds.keys
+local bar = require("modules.bar")
+
 return config
